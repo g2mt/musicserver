7c40549af561963356be519ecd18d5731fc5a7b4
g2mt <pm2mtr@gmail.com>
2026-03-21 16:45:21 +0000

    fix: db_path

--- internal/schema/config.go
+++ internal/schema/config.go
@@ -18,7 +18,7 @@ type Config struct {
 	// path where music data is stored
 	DataPath string `yaml:"data_path"`
 	// path where database and other info is stored. defaults to /var/lib/musicserver for root users, ~/.var/lib/musicserver if non-root
-	DbPath string `yaml:"data_path"`
+	DbPath string `yaml:"db_path"`
 }
 
 const SQL_DB_PATH = "./config.db"
