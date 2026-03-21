c895c8b68feccf6b98ff093ddb67fb240385fa0d
g2mt <pm2mtr@gmail.com>
2026-03-21 16:12:21 +0000

    feat: add HTTPBind, UnixBind, and DataPath fields to Config

--- internal/schema/config.go
+++ internal/schema/config.go
@@ -1,4 +1,10 @@
 package schema
 
 type Config struct {
+	// binds the HTTP API backend to this path
+	HTTPBind string
+	// binds the Unix socket API backend to this path. By default, binds to /run/musicserver/socket
+	UnixBind string
+	// path where data is stored
+	DataPath string
 }
