bfa0c0494a7b857e96f9b68916eafbcb63a382ca
g2mt <pm2mtr@gmail.com>
2026-03-21 17:45:28 +0000

    refactor: remove redundant comments in ScanTracks

--- internal/api/scantracks.go
+++ internal/api/scantracks.go
@@ -8,28 +8,22 @@ import (
 )
 
 func (i *Interface) ScanTracks() (map[string]string, error) {
-	// Walk the DataPath directory recursively
 	err := filepath.WalkDir(i.config.DataPath, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
-			// Skip files/directories with errors
 			return nil
 		}
 		if d.IsDir() {
 			return nil
 		}
 
-		// Try to load track metadata using taglib
 		track, err := taglib.LoadTrack(path)
 		if err != nil {
-			// Not an audio file or error reading metadata; skip
 			return nil
 		}
 
 		// Add track to database (ignore duplicate errors)
 		_, err = i.AddTrack(&track)
 		if err != nil {
-			// If duplicate or other error, skip
-			// (AddTrack may fail due to duplicate primary key, etc.)
 			return nil
 		}
 
@@ -40,6 +34,5 @@ func (i *Interface) ScanTracks() (map[string]string, error) {
 		return nil, err
 	}
 
-	// Return the current list of tracks after scanning
 	return i.GetTracks()
 }
