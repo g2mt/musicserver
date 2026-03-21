afde4586e2d343091e01cafcf0119e49f4ae1834
g2mt <pm2mtr@gmail.com>
2026-03-21 16:21:24 +0000

    feat: add DbPath and SQL_DB_PATH to Config struct

--- internal/schema/config.go
+++ internal/schema/config.go
@@ -5,6 +5,12 @@ type Config struct {
 	HTTPBind string `yaml:"http_bind"`
 	// binds the Unix socket API backend to this path. By default, binds to /run/musicserver/socket
 	UnixBind string `yaml:"unix_bind"`
-	// path where data is stored
+	// path where music data is stored
 	DataPath string `yaml:"data_path"`
+	// path where database and other info is stored. defaults to /var/lib/musicserver for root users, ~/.var/lib/musicserver if non-root
+	DbPath string `yaml:"data_path"`
 }
+
+const SQL_DB_PATH = "./config.db"
+
+func LoadConfig(path string) (*Config, error) {}
--- main.go
+++ main.go
@@ -10,25 +10,21 @@ import (
 )
 
 func main() {
-	// Define command line flag
 	configPath := flag.String("config", "", "path to config file")
 	flag.Parse()
 
-	// Check if config flag was provided
 	if *configPath == "" {
 		fmt.Println("Error: -config flag is required")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// Read config file
 	configData, err := os.ReadFile(*configPath)
 	if err != nil {
 		fmt.Printf("Error reading config file: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Parse YAML config
 	var config schema.Config
 	err = yaml.Unmarshal(configData, &config)
 	if err != nil {
@@ -36,6 +32,4 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Print "ok" after loading the config
-	fmt.Println("ok")
 }
