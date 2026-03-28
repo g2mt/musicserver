40d742937ebbc66be90b154424d725ac26095508
g2mt <pm2mtr@gmail.com>
2026-03-28 20:18:42 +0000

    feat: Configure Android build for API 33 and single file output

--- android/java/org/msxrv/musicserver/MainActivity.java
+++ android/java/org/msxrv/musicserver/MainActivity.java
@@ -8,10 +8,6 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 public class MainActivity extends Activity {
 	static {
@@ -28,23 +24,13 @@ public class MainActivity extends Activity {
 
 		WebView webView = (WebView)findViewById(R.id.webview);
 		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setDomStorageEnabled(true);
+		webView.getSettings().setDatabaseEnabled(true);
 		webView.setWebChromeClient(new WebChromeClient() {
 			@Override
 			public boolean onConsoleMessage(ConsoleMessage msg) {
 				Log.d("[msxrv] WebView", msg.message() + " (line " + msg.lineNumber() + ", " + msg.sourceId() + ")");
 				return true;
-			}
-		});
-		webView.setWebViewClient(new WebViewClient() {
-			@Override
-			public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-				String url = request.getUrl().toString();
-				if (url.endsWith(".js")) {
-					Log.d("[msxrv] WebView", "Intercepted JS request: " + url);
-				} else if (url.endsWith(".css")) {
-					Log.d("[msxrv] WebView", "Intercepted CSS request: " + url);
-				}
-				return super.shouldInterceptRequest(view, request);
 			}
 		});
 		webView.loadUrl("file:///android_asset/index.html");
