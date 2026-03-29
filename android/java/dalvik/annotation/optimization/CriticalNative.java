55ed2635f453767a085b08cbfe46b8a133ed0b06
g2mt <pm2mtr@gmail.com>
2026-03-29 15:46:46 +0000

    feat: add FastNative optimization

---
+++ android/java/dalvik/annotation/optimization/CriticalNative.java
@@ -0,0 +1,14 @@
+package dalvik.annotation.optimization;
+
+// Reference:
+// - https://developer.android.com/ndk/guides/jni-tips
+// - https://nickb.website/blog/speeding-up-your-android-apps-native-calls
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD})
+public @interface CriticalNative {}
