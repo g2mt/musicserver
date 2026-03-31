f188f028db593ab954c4199acc7107994e974164
g2mt <pm2mtr@gmail.com>
2026-03-31 20:27:40 +0000

    refactor: Add scroll to top for right side panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -198,11 +198,14 @@ export function App() {
   [c.leftTab, c.setLeftTab] = useState<"tracks" | "settings" | "files">(
     "tracks",
   );
-  const appLeftSide = useRef(null);
-  const appRightSide = useRef(null);
+  const appLeftSide = useRef<HTMLDivElement>(null);
+  const appRightSide = useRef<HTMLDivElement>(null);
   c.scrollToTop = () => {
     if (appLeftSide.current) {
       appLeftSide.current.scrollTop = 0;
+    }
+    if (appRightSide.current) {
+      appRightSide.current.scrollTop = 0;
     }
   };
 
