341abb8d4b579a9c17774b14873cc19bef11947a
g2mt <pm2mtr@gmail.com>
2026-04-06 01:57:00 +0000

    refactor: wrap settings in form

--- frontend/src/SettingsTab.tsx
+++ frontend/src/SettingsTab.tsx
@@ -20,81 +20,89 @@ export function SettingsTab() {
   return (
     <div className="settings-tab">
       <h2>Settings</h2>
-      <div>
-        <input
-          type="checkbox"
-          id="setting-show-blurred-cover"
-          checked={c.showBlurredCover}
-          onChange={() => {
-            c.setShowBlurredCover(!c.showBlurredCover);
-            setUnsaved(true);
-          }}
-        />
-        <label htmlFor="setting-show-blurred-cover">
-          Show blurred album cover as background in dark mode
-        </label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="setting-show-only-queue-after-enqueue"
-          checked={c.showOnlyQueueAfterEnqueue}
-          onChange={() => {
-            c.setShowOnlyQueueAfterEnqueue(!c.showOnlyQueueAfterEnqueue);
-            setUnsaved(true);
-          }}
-        />
-        <label htmlFor="setting-show-only-queue-after-enqueue">
-          Show only queue after adding tracks
-        </label>
-      </div>
-      <div>
-        <label htmlFor="setting-search-history-limit">
-          Search history limit
-        </label>
-        <input
-          type="number"
-          id="setting-search-history-limit"
-          min={0}
-          max={9999}
-          value={c.searchHistoryLimit}
-          onChange={(e) => {
-            c.setSearchHistoryLimit(parseInt(e.target.value) || 0);
-            setUnsaved(true);
-          }}
-        />
-      </div>
-      <p>
-        <button
-          className="btn"
-          disabled={!unsaved}
-          onClick={() => {
-            saveConfig(c);
-            setUnsaved(false);
-            toast.success("Settings saved");
-          }}
-        >
-          <FontAwesomeIcon icon={faSave} /> Save
-        </button>
-        <button
-          className="btn"
-          onClick={() => {
-            c.setDarkMode((b) => !b);
-            setUnsaved(true);
-          }}
-        >
-          <FontAwesomeIcon icon={c.darkMode ? faSun : faMoon} />{" "}
-          {c.darkMode ? "Light Mode" : "Dark Mode"}
-        </button>
-      </p>
-      <p>
-        <button className="btn" onClick={() => rescanFiles(false).then(() => c.onRescanned())}>
-          <FontAwesomeIcon icon={faRotate} /> Rescan Music
-        </button>
-        <button className="btn" onClick={() => rescanFiles(true).then(() => c.onRescanned())}>
-          <FontAwesomeIcon icon={faRotate} /> Rescan Music (force update)
-        </button>
-      </p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <input
+            type="checkbox"
+            id="setting-show-blurred-cover"
+            checked={c.showBlurredCover}
+            onChange={() => {
+              c.setShowBlurredCover(!c.showBlurredCover);
+              setUnsaved(true);
+            }}
+          />
+          <label htmlFor="setting-show-blurred-cover">
+            Show blurred album cover as background in dark mode
+          </label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="setting-show-only-queue-after-enqueue"
+            checked={c.showOnlyQueueAfterEnqueue}
+            onChange={() => {
+              c.setShowOnlyQueueAfterEnqueue(!c.showOnlyQueueAfterEnqueue);
+              setUnsaved(true);
+            }}
+          />
+          <label htmlFor="setting-show-only-queue-after-enqueue">
+            Show only queue after adding tracks
+          </label>
+        </div>
+        <div>
+          <label htmlFor="setting-search-history-limit">
+            Search history limit
+          </label>
+          <input
+            type="number"
+            id="setting-search-history-limit"
+            min={0}
+            max={9999}
+            value={c.searchHistoryLimit}
+            onChange={(e) => {
+              c.setSearchHistoryLimit(parseInt(e.target.value) || 0);
+              setUnsaved(true);
+            }}
+          />
+        </div>
+        <p>
+          <button
+            className="btn"
+            disabled={!unsaved}
+            onClick={() => {
+              saveConfig(c);
+              setUnsaved(false);
+              toast.success("Settings saved");
+            }}
+          >
+            <FontAwesomeIcon icon={faSave} /> Save
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              c.setDarkMode((b) => !b);
+              setUnsaved(true);
+            }}
+          >
+            <FontAwesomeIcon icon={c.darkMode ? faSun : faMoon} />{" "}
+            {c.darkMode ? "Light Mode" : "Dark Mode"}
+          </button>
+        </p>
+        <p>
+          <button
+            className="btn"
+            onClick={() => rescanFiles(false).then(() => c.onRescanned())}
+          >
+            <FontAwesomeIcon icon={faRotate} /> Rescan Music
+          </button>
+          <button
+            className="btn"
+            onClick={() => rescanFiles(true).then(() => c.onRescanned())}
+          >
+            <FontAwesomeIcon icon={faRotate} /> Rescan Music (force update)
+          </button>
+        </p>
+      </form>
       <hr />
 
       <h2>Server properties</h2>
