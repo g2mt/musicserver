062b25f719d3210ed5c537af1c30432e4119b855
g2mt <pm2mtr@gmail.com>
2026-04-21 03:35:54 +0000

    style: fix indent

--- internal/api/interface_trackcache.go
+++ internal/api/interface_trackcache.go
@@ -12,18 +12,18 @@ type trackCacheData struct {
 
 func (i *Interface) initCacheDb() error {
 	_, err := i.cacheDb.Exec(`
-			CREATE TABLE IF NOT EXISTS cover_cache (
-				path TEXT PRIMARY KEY,
-				data BLOB NOT NULL,
-				mime_type TEXT NOT NULL,
-				timestamp INTEGER NOT NULL DEFAULT 0
-			);
-			CREATE TABLE IF NOT EXISTS stats (
-				key TEXT PRIMARY KEY,
-				value INTEGER NOT NULL
-			);
-			INSERT OR IGNORE INTO stats (key, value) VALUES ('size', 0);
-		`)
+		CREATE TABLE IF NOT EXISTS cover_cache (
+			path TEXT PRIMARY KEY,
+			data BLOB NOT NULL,
+			mime_type TEXT NOT NULL,
+			timestamp INTEGER NOT NULL DEFAULT 0
+		);
+		CREATE TABLE IF NOT EXISTS stats (
+			key TEXT PRIMARY KEY,
+			value INTEGER NOT NULL
+		);
+		INSERT OR IGNORE INTO stats (key, value) VALUES ('size', 0);
+	`)
 	return err
 }
 
