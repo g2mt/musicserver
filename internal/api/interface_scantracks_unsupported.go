795225ce34e694d92674a029d426e315f0a28188
g2mt <pm2mtr@gmail.com>
2026-04-24 16:33:45 +0000

    fix: replace panic with error returns in ScanTracks and WatchDataDir

--- internal/api/interface_scantracks_unsupported.go
+++ internal/api/interface_scantracks_unsupported.go
@@ -2,10 +2,12 @@
 
 package api
 
+import "errors"
+
 func (i *Interface) ScanTracks(args ...any) (addedFiles int, err error) {
-	panic("ScanTracks called by Go code")
+	return 0, errors.New("ScanTracks called by Go code")
 }
 
 func (i *Interface) WatchDataDir() error {
-	panic("WatchDataDir called by Go code")
+	return errors.New("WatchDataDir called by Go code")
 }
